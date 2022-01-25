4accba71f421267eba66c6cbe7baabd86b198845
tighidet <tighidet.zineddine@gmail.com>
2022-01-25 21:11:52 +0100

    init

---
+++ server.R
@@ -0,0 +1,65 @@
+library(shiny)
+library(ggplot2)
+library(tidyverse)
+library(GGally)
+library(reshape2)
+library(ggthemes)
+library(Factoshiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+
+    dataset <- eventReactive(input$load_data, {
+        inFile <- input$file
+        if(is.null(inFile)) return(NULL)
+        read.csv(inFile$datapath, header=input$file_has_header, stringsAsFactors = TRUE)
+    })
+    
+    output$quant_table <- renderTable({
+        dataset() %>%
+            select_if(is.numeric)
+    }, colnames = TRUE)
+    
+    output$cat_table <- renderTable({
+        dataset() %>%
+            select_if(is.factor)
+    })
+    
+    output$boxplots <- renderPlot({
+        dataset() %>%
+            select_if(is.numeric) %>%
+            melt() %>%
+            ggplot() +
+            geom_boxplot(outlier.colour = "red", aes(x = value, fill = variable)) +
+            facet_wrap(~variable, scales = "free") +
+            labs(x = NULL) +
+            theme_solarized()
+    })
+    
+    
+    output$barplots <- renderPlot({
+        categ_data <- dataset() %>%
+            select_if(is.factor)
+        categ_data %>%
+        melt(id.vars=names(categ_data)[1]) %>%
+        ggplot(aes(x = value, fill=variable)) +
+        geom_bar() +
+        facet_wrap(~variable, scales = "free") +
+        labs(x = NULL) +
+        theme_solarized()
+    })
+    
+    output$pca_var <- renderPlot({
+        res.PCA<-PCA(dataset() %>%
+                         select_if(is.numeric),quali.sup=c(1,2,3,4,5,12),graph=FALSE)
+        plot.PCA(res.PCA,choix='var',title="Graphe des variables de l'ACP")
+    })
+    
+    output$pca_individ <- renderPlot({
+        res.PCA<-PCA(dataset() %>%
+                         select_if(is.numeric),quali.sup=c(1,2,3,4,5,12),graph=FALSE)
+        plot.PCA(res.PCA,title="Graphe des individus de l'ACP")
+    })
+    
+
+})
---
+++ ui.R
@@ -0,0 +1,101 @@
+library(shiny)
+library(shinythemes)
+
+# Define UI for application that draws a histogram
+shinyUI(
+        navbarPage("Dashboard",
+                   tabPanel(
+                       "Data Cleaning",
+                       sidebarPanel(
+                               fluidRow(
+                                      fileInput(inputId = "file", label = "Load a data file" , accept = c("text/plain", ".csv")),
+                               ),
+                               fluidRow(
+                                   checkboxInput(inputId = "file_has_header", label = "File has header", value = TRUE)
+                               ),
+                               fluidRow(
+                                      selectInput(inputId = "nas_choice", label=h3("NAs values"),
+                                                  choices=list("Remove" = 1,
+                                                               "Fill with mean" = 2,
+                                                               "Use KMeans to fill" = 3),
+                                                  selected = 1)
+                               ),
+                               fluidRow(
+                                      checkboxGroupInput(inputId = "pre_pros", label = h3("Pre-Processing"), 
+                                                         choices = list("Normalize Data" = 1,
+                                                                        "Balance Data" = 2,
+                                                                        "Remove Outiliers" = 3),
+                                                         selected = 1)
+                                ),
+                               fluidRow(
+                                    numericInput("n_sample", label = h3("Sample size"), value = -1),
+                               ),
+                               fluidRow(
+                                   column(3,
+                                          actionButton(inputId="load_data", label="Load Dataset"),
+                                          offset = 3
+                                          )
+    
+                               )
+                       ),
+                       mainPanel(
+                           tabsetPanel(
+                               tabPanel(
+                                   "Quantitative Variables",
+                                   fluidRow(
+                                       column(8,
+                                              tableOutput(outputId = "quant_table"),
+                                              offset = 1)
+                                   )
+                               ),
+                               # change style:    
+                               tags$head(tags$style("#quant_table table {background-color: #8bb8bf; }", media="screen", type="text/css")),
+                               tabPanel(
+                                   "Categorical Variables",
+                                   fluidRow(
+                                       column(8,
+                                              tableOutput(outputId = "cat_table"),
+                                              offset = 1)
+                                   )
+                               ),
+                               # change style:    
+                               tags$head(tags$style("#cat_table table {background-color: #8bb8bf; }", media="screen", type="text/css"))
+                           )
+                       )                   
+                       ),
+                   tabPanel(
+                       "Data Visualization",
+                           tabsetPanel(
+                               tabPanel(
+                                   "Quantitative Variables",
+                                   fluidRow(
+                                       column(8,
+                                              plotOutput(outputId = "boxplots"),
+                                              offset = 1)
+                                   )
+                               ),
+                               tabPanel(
+                                   "Categorical Variables",
+                                   fluidRow(
+                                        plotOutput(outputId = "barplots")
+                                   )
+                               ),
+                               tabPanel(
+                                 "PCA",
+                                 fluidRow(
+                                   column(6,
+                                          plotOutput(outputId = "pca_var")
+                                          ),
+                                   column(6,
+                                          plotOutput(outputId = "pca_individ")
+                                          )
+                                   
+                                 )
+                               )
+                           )
+                       ),
+                   tabPanel("Train Models"),
+                   theme=shinytheme("cosmo"),
+                   tags$head(tags$style("body {font-family: Times New Roman; font-size: 300%; }", media="screen", type="text/css"))
+        )
+)
